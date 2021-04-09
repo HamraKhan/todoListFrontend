ee5fab6d5cf035e7650b0e5dc3a3c9bd946bba24
Hamra Khan <hamra111@gmail.com>
2021-04-09 22:42:33 +0100

    Added activity class and imported it in the dashboard page.
    Updated the creation and read logic to handle the new & old data model for the to do list activities.
    
    Cleaned up code. Defined the premise for the activitiy toggle action.

---
+++ activity.js
@@ -0,0 +1,10 @@
+class Activity {
+    title = null;
+    checked = false;
+    status =  'ACTiVE';
+    constructor(title, checked, status) {
+        this.title = title;
+        this.checked = checked;
+        this.status = status;
+    }
+}
